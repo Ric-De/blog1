9b19ab47886abab57f475843e19ee29efdbd8e80
Ric-De <ricdellazari@gmail.com>
2023-12-20 15:38:13 +0100

    Blade Views Added

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -7,17 +7,17 @@ use Illuminate\Http\Request;
 class CursoController extends Controller
 {
     public function index(){
-        return "Bienvenido a la pagina cursos";
-
+        //return "Bienvenido a la pagina cursos";
+        return view('cursos.index');
     }
 
     public function create(){
-        return "En esta pagina podras crear un curso";
-
+        //return "En esta pagina podras crear un curso";
+        return view('cursos.create');
     }
 
     public function show($curso){
-        return "Bienvenido al curso: $curso";
-
+        //return "Bienvenido al curso: $curso";
+        return view('cursos.show', ['curso' => $curso]);
     }
 }
---
+++ resources/views/cursos/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Cursos create')
+
+@section('content')
+    <h1>En esta pagina podras crear un curso</h1>
+@endsection
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>En esta pagina podras crear un curso</h1>
+</body>
+</html> -->
