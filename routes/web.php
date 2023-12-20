7acb1a655f636d9026726353718e653e8adb3960
Ric-De <ricdellazari@gmail.com>
2023-12-20 14:28:28 +0100

    Routes and Controllers finished

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CursoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,47 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//This is the final clean way to do the routes using the controllers.
+
+Route::get('/',HomeController::class);
+
+Route::controller(CursoController::class)->group(function(){
+    Route::get('cursos', 'index');
+    Route::get('cursos/create', 'create');
+    Route::get('cursos/{curso}', 'show');
+
 });
+
+/* Route::get('cursos', [CursoController::class, 'index']);
+Route::get('cursos/create', [CursoController::class, 'create']);
+Route::get('cursos/{curso}', [CursoController::class, 'show']); */
+
+
+
+/* Route::get('/', function () {
+    //return view('welcome');
+    return "Bienvenido a la pagina principal";
+}); */
+
+/* Route::get('cursos', function () {
+    return "Bienvenido a la pagina cursos";
+});
+
+Route::get('cursos/create', function () {
+    return "En esta pagina podras crear un curso";
+});
+
+Route::get('cursos/{curso}', function ($curso) {
+    return "Bienvenido al curso: $curso";
+}); */
+
+//Good route. But all this logic with if and else need to be inside a controller. Need to create a controller to do that.
+/* Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+    if($categoria){
+        return "Bienvenido al curso $curso de la categoria $categoria";
+    } else{
+        return "Bienvenido al curso: $curso";
+    }
+}); */
+
+
